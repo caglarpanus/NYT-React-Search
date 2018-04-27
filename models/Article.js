7176838a4c2445d22d66d2e14d045be3e5c48ff3
caglarpanus <panuscaglar@gmail.com>
2018-04-27 13:38:20 -0400

    server side is OK

--- models/Article.js
+++ models/Article.js
@@ -1,29 +1,25 @@
-import { connect } from "net";
-import { mongo } from "mongoose";
 
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ArticleSchema = new Schema({
-   title:{
-       type:String,
-       trim:true,
-       required:"Title is required"
-   },
-   date:{
-       type:Date,
-       default:Date.now,
-       required:"Date is required"
-   },
-   url:{
-       type:String,
-       required:true,
-       required:"URL is required",
-       unique:true
-   }
+    title: {
+        type: String,
+        required:true
+    },
+    url: {
+        type: String,
+        required:true
+    },
+    date: {
+        type: Date,
+        required:true
+    },
+    snippet: {
+        type:String,
+        required: true
+    }
 });
 
-//creates the model
-var Article = mongoose.model("Article", ArticleSchema);
-
+const Article = mongoose.model("Article", ArticleSchema);
 module.exports = Article;
